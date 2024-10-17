72e77c0b8414be3843a47c4d88675041ab2f3418
ettoreMB <ettore.barbosa@gmail.com>
2024-10-16 21:02:18 -0300

    created commom folder
    entity
    agregaterrot

--- src/@core/event/entities/customer.entity.ts
+++ src/@core/event/entities/customer.entity.ts
@@ -1,15 +1,19 @@
+import { AggregateRoot } from "src/@core/commom/domain/aggregate-root";
+import { Entity } from "src/@core/commom/domain/entity";
+
 type CustomerConstrucorProps = {
   id?: string;
   cpf: string;
   name: string;
 };
 
-export class Customer {
+export class Customer extends AggregateRoot{
   id: string;
   cpf: string;
   name: string;
 
   constructor(props: CustomerConstrucorProps) {
+    super();
     this.id = props.id;
     this.cpf = props.cpf;
     this.name = props.name;
